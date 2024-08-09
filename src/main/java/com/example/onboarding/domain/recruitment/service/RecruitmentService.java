81152d0f1c7f2c2230f54812e48b70ce6a7d9843
allllfo <mirae4292@gmail.com>
2024-08-10 04:10:07 +0900

    docs: recruitmentService에 주석추가

--- src/main/java/com/example/onboarding/domain/recruitment/service/RecruitmentService.java
+++ src/main/java/com/example/onboarding/domain/recruitment/service/RecruitmentService.java
@@ -22,6 +22,7 @@ public class RecruitmentService {
     private final RecruitmentRepository recruitmentRepository;
     private final CompanyRepository companyRepository;
 
+    //채용공고 등록
     public RecruitmentSaveOneResponse saveRecruitment(RecruitmentSaveOneRequest request) {
         Company findCompany = findCompanyById(request.getCompanyId());
 
@@ -37,11 +38,13 @@ public class RecruitmentService {
         return RecruitmentSaveOneResponse.from(recruitment);
     }
 
+    //채용공고 수정
     public void updateRecruitment(RecruitmentUpdateOneRequest request, Integer recruitmentId) {
         Recruitment findRecruitment = findRecruitmentById(recruitmentId);
         findRecruitment.update(request.getPosition(), request.getReward(), request.getDescription(), request.getStack());
     }
 
+    //전체 채용공고 조회
     public List<RecruitmentFindAllResponse> findAllRecruitments() {
         List<RecruitmentFindAllResponse> response = recruitmentRepository.findAll().stream()
                 .map(recruitment -> {
@@ -50,6 +53,7 @@ public class RecruitmentService {
         return response;
     }
 
+    //채용공고 검색
     public List<RecruitmentSearchAllResponse> searchAllRecruitments(String searchTag) {
         List<RecruitmentSearchAllResponse> response = recruitmentRepository.findBySearchTag(searchTag)
                 .stream()
@@ -59,6 +63,7 @@ public class RecruitmentService {
         return response;
     }
 
+    //상세페이지 조회
     public RecruitmentDetailFindOneResponse findRecruitmentDetail(Integer recruitmentId) {
         Recruitment findRecruitment = findRecruitmentById(recruitmentId);
         List<Integer> anotherRecruitments = recruitmentRepository.findByCompanyId(findRecruitment.getCompany().getId());
@@ -66,18 +71,19 @@ public class RecruitmentService {
         return RecruitmentDetailFindOneResponse.of(findRecruitment, anotherRecruitments);
     }
 
-
+    //채용공고 삭제
     public void deleteRecruitment(Integer recruitmentId) {
         Recruitment findRecruitment = findRecruitmentById(recruitmentId);
         recruitmentRepository.delete(findRecruitment);
     }
 
-
+    //comapnyId값으로 company찾기
     private Company findCompanyById(Integer companyId){
         return companyRepository.findById(companyId)
                 .orElseThrow(()-> new CustomException(ErrorCode.NOT_FOUND_COMPANY));
     }
 
+    //recruitmentId값으로 recruitment찾기
     private Recruitment findRecruitmentById(Integer recruitmentId){
         return recruitmentRepository.findById(recruitmentId)
                 .orElseThrow(()-> new CustomException(ErrorCode.NOT_FOUND_RECRUITMENT));
